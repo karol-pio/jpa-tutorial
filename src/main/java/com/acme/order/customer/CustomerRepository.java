e8affefd670bb635e7bac77955802458ffaceafe
KWojtaszewski <karol.wojtaszewski@stazysta.comarch.pl>
2015-07-09 14:50:14 +0200

    Add custom method in CustomerRepository

--- src/main/java/com/acme/order/customer/CustomerRepository.java
+++ src/main/java/com/acme/order/customer/CustomerRepository.java
@@ -1,6 +1,10 @@
 package com.acme.order.customer;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface CustomerRepository extends CrudRepository<Customer, Long> {
+	
+	List<Customer> findByCustomerTypeName(String name);
 }
